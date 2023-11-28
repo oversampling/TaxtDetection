ef1413fee18fbf3447d49e2850b57d4a286ad96b
oversampling <chan1992241@gmail.com>
2023-11-28 16:37:37 +0800

    Feat: Client Side Streaming

--- connector/main.py
+++ connector/main.py
@@ -1,4 +1,5 @@
 from typing import Union
+import requests
 import uvicorn
 import os
 from fastapi import FastAPI, Request
@@ -6,16 +7,28 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
+
 load_dotenv()
 CAM_URL = os.getenv("CAM_URL")
-print(CAM_URL)
+RECONG_URL = os.getenv("RECONG_URL")
+
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/ipcam", response_class=HTMLResponse)
-def read_root(request: Request):    
-    return templates.TemplateResponse("item.html", {"request": request, "cam_url": CAM_URL})
+def tag_recong(request: Request, tag: Union[str, str]):   
+    return templates.TemplateResponse("item.html", {"request": request, "img_url": "/static/img.jpg", "tag": tag})
 
-if __name__ == "__main__":
+@app.post("/stream", status_code=200)
+def accessing_camera():
+    photo = requests.get(CAM_URL)
+    with open("static/img.jpg", "wb") as f:
+        f.write(photo)
+    files = {'file': open('img.jpg', 'rb')}
+    response = requests.post(RECONG_URL, files=files)
+    return "OK"
+
+if __name__ == "__main__": 
     uvicorn.run(app, port=8080)
